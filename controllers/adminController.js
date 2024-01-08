e6f6bb2df00e773b01b86264cc67a34ee6f60f46
Rahul Kumar <kumar0206rahul@gmail.com>
2024-01-08 23:19:16 +0530

    added admin forget and reset password functionality

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,56 @@
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const nodemailer = require("nodemailer");
+const randomstring = require("randomstring");
+require("dotenv").config();
+
+const securePassword = async(password)=>{
+    try {
+        
+        const passwordHash = await bcrypt.hash(password, 10);
+        return passwordHash;
+
+
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+//for reset password send mail
+const sendResetPasswordMail = async(name, email, token)=>{
+    try {
+        
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 587,
+            secure: false,
+            requireTLS: true,
+            auth:{
+                user:process.env.USER,
+                pass: process.env.PASS
+            }
+        });
+
+        const mailOptions = {
+            from: process.env.USER,
+            to: email,
+            subject: 'For Reset Password ',
+            html: '<p>Hi, '+name+', please click here to <a href="http://localhost:3000/admin/forget-password?token='+token+'"> Reset </a> your password. </p>'
+        }
+
+        transporter.sendMail(mailOptions, function(error, info){
+            if(error){
+                console.log(error);
+            }
+            else{
+                console.log("email has been sent:- ", info.response);
+            }
+        });
+
+    } catch (error) {
+        console.log(error.message);
+    }
+}
 
 const loadLogin = async(req,res)=>{
     try {
@@ -70,9 +121,91 @@ const logout = async(req,res)=>{
     }
 }
 
+const forgetLoad = async(req,res)=>{
+    try {
+
+        res.render('forget');
+        
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+const forgetVerify = async(req,res)=>{
+    try {
+
+        const email = req.body.email;
+        const userData = await User.findOne({email:email});
+
+        if(userData){
+
+            if(userData.is_admin === 0){
+                res.render('forget',{message: 'Email is Incorrect'});
+            }
+            else{
+
+                const randomString = randomstring.generate();
+                const updatedData = await User.updateOne({email:email}, {$set:{token:randomString}});
+
+                sendResetPasswordMail(userData.name, userData.email, randomString);
+                res.render('forget',{message:"Please Check Your mail to reset your password."});
+
+            }
+
+        }
+        else{
+            res.render('forget',{message: 'Email is Incorrect'});
+        }
+        
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+const forgetPasswordLoad = async(req,res)=>{
+    try {
+
+        const token = req.query.token;
+
+        const tokenData = await User.findOne({token:token});
+
+        if(tokenData){
+            res.render('forget-password',{user_id: tokenData._id});
+        }
+        else{
+            res.render('404', {message:"Invalid Link"});
+        }
+
+        
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+const resetPassword = async(req,res)=>{
+    try {
+
+        const password = req.body.password;
+        const user_id = req.body.user_id;
+
+        const securePass = await securePassword(password);
+
+        const updatedData = await User.findByIdAndUpdate({ _id:user_id},{$set:{password:securePass, token:''}});
+
+        res.redirect('/admin');
+
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
 module.exports = {
     loadLogin,
     verifyLogin,
     loadDashboard,
-    logout
+    logout,
+    forgetLoad,
+    forgetVerify,
+    forgetPasswordLoad,
+    resetPassword
 }
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const admin_route = express();
-
 const session = require("express-session");
-const config = require("../config/config.js");
+
+const config = require("../config/config");
+
 admin_route.use(session({secret:config.SESSIONSECRET}));
 
-const bodyParser = require("body-parser");
-admin_route.use(bodyParser.json());
-admin_route.use(bodyParser.urlencoded({extended:true}));
+const auth = require("../middleware/adminAuth");
 
 admin_route.set('view engine', 'ejs');
 admin_route.set('views','./views/admin');
 
-const auth = require("../middleware/adminAuth");
+const bodyParser = require("body-parser");
+admin_route.use(bodyParser.json());
+admin_route.use(bodyParser.urlencoded({extended:true}));
 
 const adminController = require("../controllers/adminController");
 
@@ -23,6 +24,14 @@ admin_route.post('/',adminController.verifyLogin);
 admin_route.get('/adminHome',auth.isLogin , adminController.loadDashboard);
 
 admin_route.get('/logout',auth.isLogin, adminController.logout);
+
+admin_route.get('/forget', auth.isLogout, adminController.forgetLoad);
+
+admin_route.post('/forget', adminController.forgetVerify);
+
+admin_route.get('/forget-password',auth.isLogout, adminController.forgetPasswordLoad);
+
+admin_route.post('/forget-password',adminController.resetPassword);
 
 
 admin_route.get('*', function(req,res){
